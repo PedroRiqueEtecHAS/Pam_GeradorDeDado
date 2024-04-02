d17bb7700d86e7d721452ad38c0b5908e24ec2fd
Rique_Pedro <pedroriqueetec3@gmail.com>
2024-04-02 12:32:12 -0300

    trabalho concluído

---
+++ AtividadePAM2/AtividadePAM2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AtividadePAM2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AtividadePAM2/AtividadePAM2/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace AtividadePAM2
+{
+    public partial class MainPage : ContentPage
+    {
+        int ValorDado = 0;
+        string guardar = null;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRolar_Clicked(object sender, EventArgs e)
+        {
+            ValorDado = Convert.ToInt32((string)ValorP.SelectedItem);
+            Random rnd = new Random();
+            int Calcular = rnd.Next(1,ValorDado + 1);
+            Valor.Text = Convert.ToString(Calcular);
+            
+        }
+    }
+
+}
