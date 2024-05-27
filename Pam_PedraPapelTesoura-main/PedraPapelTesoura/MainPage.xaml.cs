22db216583f2fa8644c9212b2833e2d2d64a6c22
Henrique <henrique.rique200611@gmail.com>
2024-05-27 17:57:44 -0400

    estilização completa

--- Pam_PedraPapelTesoura-main/PedraPapelTesoura/MainPage.xaml.cs
+++ Pam_PedraPapelTesoura-main/PedraPapelTesoura/MainPage.xaml.cs
@@ -21,29 +21,62 @@
             string jogador1 = player.Text;
 
 
+            if(escolhaPlayer == "Pedra")
+            {
+                imgPlayer.Source = "pedraE.png";
+            }
+            if (escolhaPlayer == "Papel")
+            {
+                imgPlayer.Source = "papelE.png";
+                imgPlayer.WidthRequest = 200;
+                imgPlayer.Margin = 30;
+            }
+            if (escolhaPlayer == "Tesoura")
+            {
+                imgPlayer.Source = "TesouraE.png";
+            }
             if (Calcular == 1)
             {
                 resultadoBot.Text = pedra;
+                imgBot.Source = "pedraD.png";
+                imgBot.WidthRequest = 300;
+                imgBot.Margin = 0;
+
             }
             if (Calcular == 2)
             {
                 resultadoBot.Text = papel;
+                imgBot.Source = "papelD.png";
+                imgBot.WidthRequest = 200;
+                imgBot.Margin = 50;
             }
             if (Calcular == 3)
             {
                 resultadoBot.Text = tesoura;
+                imgBot.Source = "tesouraD.png";
+                imgBot.WidthRequest = 300;
+                imgBot.Margin = 0;
             }
             if (escolhaPlayer == "Pedra") 
             {
                 resultadoPlayer.Text = pedra;
+                imgPlayer.Source = "pedraD.png";
+                imgPlayer.WidthRequest = 300;
+                imgPlayer.Margin = 0;
             }
             if (escolhaPlayer == "Papel")
             {
                 resultadoPlayer.Text =  papel;
+                imgPlayer.Source = "papelE.png";
+                imgPlayer.WidthRequest = 200;
+                imgPlayer.Margin = 50;
             }
             if (escolhaPlayer == "Tesoura")
             {
                 resultadoPlayer.Text = tesoura;
+                imgPlayer.Source = "TesouraE.png";
+                imgPlayer.WidthRequest = 300;
+                imgPlayer.Margin = 0;
             }
             if (resultadoPlayer.Text == pedra && resultadoBot.Text == papel)
             {
